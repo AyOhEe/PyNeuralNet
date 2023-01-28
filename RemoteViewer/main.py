9c657d45e9ad3d9d1b76f67fd2d9a0a573c5676c
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-01-28 21:56:14 +0000

    Removed RemoteViewer

--- RemoteViewer/main.py
+++
@@ -1,9 +0,0 @@
-#End goal for the viewer
-#   -can remote connect to any instance of a simulation and observe the active networks
-#       -can run network at normal speed or step through one at a time, or at a certain speed
-#   -can open a genome and display the structure
-#       -can edit and save opened genomes
-#Would also like, for any simulation i make, for it to be made in two parts:
-#   -the server (handles simulating the environment)
-#   -the client (handles observing and controlling the environment/server itself,
-#       hyperparameters, speed, etc...)
